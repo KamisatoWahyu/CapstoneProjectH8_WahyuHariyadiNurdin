c583933f0c9becba67e4dd4462c6dcabb785be19
Wahyu & Furina hehehe <161020966+KamisatoWahyu@users.noreply.github.com>
2024-12-18 00:06:51 +0700

    Second change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+
+import lineClamp from "@tailwindcss/line-clamp";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/line-clamp")],
+  plugins: [lineClamp],
 };
